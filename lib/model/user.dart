664e2548900ec1fea9d435e9e52f1c891d77c75f
albert <albertmasa2@gmail.com>
2020-04-29 15:14:59 +0200

    Added initial model structure

---
+++ lib/model/user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class User {
+  String displayName;
+  String username;
+  // List<Poll> polls;
+
+  User({
+    @required this.displayName,
+    @required this.username,
+  });
+}
