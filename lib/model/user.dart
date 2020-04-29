914efc6172261cd441d4516cb1abfe40016fb974
albert <albertmasa2@gmail.com>
2020-04-29 16:08:24 +0200

    Added user id to model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
 
 class User {
+  String id;
   String displayName;
   String username;
   List<Poll> polls;
 
   User({
+    @required this.id,
     @required this.displayName,
     @required this.username,
     this.polls,
