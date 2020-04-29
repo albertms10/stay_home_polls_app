486a10b0d87714f28c2395e42bdf063f3ab1852f
albert <albertmasa2@gmail.com>
2020-04-29 15:19:29 +0200

    Added polls field in User model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:stay_home_polls_app/model/poll.dart';
 
 class User {
   String displayName;
   String username;
-  // List<Poll> polls;
+  List<Poll> polls;
 
   User({
     @required this.displayName,
     @required this.username,
+    this.polls,
   });
 }
