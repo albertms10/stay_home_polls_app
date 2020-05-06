d57dc9693ce197cc581a82ece8f25cd2b53d4f1b
albert <albertmasa2@gmail.com>
2020-05-06 13:34:12 +0200

    Implemented ChoicePollAction options
    Co-authored-by: veralegret <veralegret@gmail.com>

--- lib/widgets/choice_poll_action.dart
+++ lib/widgets/choice_poll_action.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
+import 'package:stay_home_polls_app/model/user.dart';
 
-class ChoicePollAction extends StatelessWidget {
+class ChoicePollAction extends StatefulWidget {
   final ChoicePoll choicePoll;
 
   ChoicePollAction({@required this.choicePoll});
 
   @override
+  _ChoicePollActionState createState() => _ChoicePollActionState();
+}
+
+class _ChoicePollActionState extends State<ChoicePollAction> {
+  int _selectedValue;
+  bool voted;
+
+  @override
+  void initState() {
+    super.initState();
+    voted = false;
+  }
+
+  _vote() => voted
+      ? null
+      : (value) {
+          setState(() {
+            _selectedValue = value;
+            voted = true;
+          });
+          User.vote(widget.choicePoll, value);
+        };
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        for (int i = 0; i < widget.choicePoll.optionsVoteCount.length; i++)
+          RadioListTile<int>(
+            title: Text(widget.choicePoll.options[i]),
+            secondary: voted
+                ? Text(
+                    '${(widget.choicePoll.optionsVoteCount[i] / widget.choicePoll.totalCount * 100).round()}%')
+                : null,
+            value: i,
+            groupValue: _selectedValue,
+            onChanged: _vote(),
+          )
+      ],
+    );
   }
 }
