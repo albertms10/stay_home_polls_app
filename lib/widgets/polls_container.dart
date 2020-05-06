6d6c8e12da3750677226f2091b86103281a4fc2f
albert <albertmasa2@gmail.com>
2020-05-06 13:39:18 +0200

    Refactored PollsContainer Stream function name
    Co-authored-by: veralegret <veralegret@gmail.com>

--- lib/widgets/polls_container.dart
+++ lib/widgets/polls_container.dart
@@ -3,10 +3,14 @@ import 'package:stay_home_polls_app/model/poll.dart';
 import 'package:stay_home_polls_app/widgets/polls_list.dart';
 
 class PollsContainer extends StatelessWidget {
+  final Stream<List<Poll>> streamPollsList;
+
+  PollsContainer(this.streamPollsList);
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Poll>>(
-      stream: pollListSnapshots(),
+      stream: streamPollsList,
       builder: (context, AsyncSnapshot<List<Poll>> snapshot) {
         if (snapshot.hasError)
           return Center(child: Text('ERROR: ${snapshot.error.toString()}'));
