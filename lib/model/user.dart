2cecd8d74391a1bd97a690597202b8a81888cb78
albert <albertmasa2@gmail.com>
2020-05-06 13:36:51 +0200

    Implemented User (static) vote method
    Co-authored-by: veralegret <veralegret@gmail.com>

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
 
@@ -25,4 +26,26 @@ class User {
         "userName": userName,
         "polls": polls,
       };
+
+  static vote(Poll poll, int value, [bool isAuth = false]) {
+    // TODO: Refactor this
+    String userId = 'Ap8s7eym7sY32CnuGIgM';
+
+    Firestore.instance
+        .collection('users/$userId/polls')
+        .document(poll.id)
+        .setData({
+      'isAuth': isAuth,
+      'type': poll is SliderPoll ? 'slider' : 'choice',
+      'voteValue': value,
+    });
+
+    Firestore.instance.collection('polls').document(poll.id).updateData({
+      'voteCount': poll.voteCount + 1,
+      if (poll is ChoicePoll)
+      // TODO: Refactor replace method (?)
+        'optionsVoteCount': poll.optionsVoteCount
+          ..replaceRange(value, value + 1, [poll.optionsVoteCount[value] + 1])
+    });
+  }
 }
