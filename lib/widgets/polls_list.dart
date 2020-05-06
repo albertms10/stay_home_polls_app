e196482bc8133a6c0bd5f6b3aec7d8aa4304b907
albert <albertmasa2@gmail.com>
2020-05-06 14:13:23 +0200

    Implemented Dismissible PollTile

--- lib/widgets/polls_list.dart
+++ lib/widgets/polls_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
+import 'package:stay_home_polls_app/model/user.dart';
 import 'package:stay_home_polls_app/widgets/poll_tile.dart';
 
 class PollsList extends StatelessWidget {
@@ -11,7 +12,14 @@ class PollsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: polls.length,
-      itemBuilder: (context, int index) => PollTile(poll: polls[index]),
+      itemBuilder: (context, int index) => Dismissible(
+        key: Key(polls[index].id),
+        child: PollTile(poll: polls[index]),
+        onDismissed: (_) {
+          User.dismiss(polls[index]);
+          polls.removeAt(index);
+        },
+      ),
     );
   }
 }
