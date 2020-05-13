bd6e49d27c0b3cec92a2a7113c62e6af92551ead
albert <albertmasa2@gmail.com>
2020-05-13 14:12:54 +0200

    Refactored PollsContainer Widget with custom StreamBuilder filterCallback function
    Co-authored-by: veralegret <veralegret@gmail.com>

--- lib/widgets/polls_container.dart
+++ lib/widgets/polls_container.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
+import 'package:stay_home_polls_app/model/user.dart';
 import 'package:stay_home_polls_app/widgets/polls_list.dart';
 
 class PollsContainer extends StatelessWidget {
   final Stream<List<Poll>> streamPollsList;
+  final Function filterCallback;
 
-  PollsContainer(this.streamPollsList);
+  PollsContainer({this.streamPollsList, this.filterCallback});
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<List<Poll>>(
-      stream: streamPollsList,
-      builder: (context, AsyncSnapshot<List<Poll>> snapshot) {
+  StreamBuilder _streamBuilder(stream, Function callback) {
+    return StreamBuilder(
+      stream: stream,
+      builder: (context, AsyncSnapshot snapshot) {
         if (snapshot.hasError)
-          return Center(child: Text('ERROR: ${snapshot.error.toString()}'));
+          return Center(child: Text('Error: ${snapshot.error}'));
 
         switch (snapshot.connectionState) {
           case ConnectionState.waiting:
             return Center(child: CircularProgressIndicator());
           case ConnectionState.active:
-            return PollsList(polls: snapshot.data);
+            return callback(snapshot.data);
           case ConnectionState.done:
-            return Center(child: Text("done??"));
+            return Center(child: Text("Connection done"));
           case ConnectionState.none:
           default:
-            return Center(child: Text("no hi ha stream??"));
+            return Center(child: Text("No stream"));
         }
       },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = User(id: 'Ap8s7eym7sY32CnuGIgM', displayName: 'Albert');
+
+    return _streamBuilder(streamPollsList, (List<Poll> polls) {
+      return _streamBuilder(user.pollsSnapshots(), (List<Poll> userPolls) {
+        return PollsList(
+            polls: filterCallback != null
+                ? filterCallback(polls, userPolls)
+                : polls);
+      });
+    });
   }
 }
