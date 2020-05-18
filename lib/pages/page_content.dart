ba98a05ae54f44785abd803a5885bbfc76fe7386
albert <albertmasa2@gmail.com>
2020-05-18 22:02:26 +0200

    Refactored empty polls list message with a Provider

--- lib/pages/page_content.dart
+++ lib/pages/page_content.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PageContent extends StatelessWidget {
   final List<Tab> tabs;
   final List<Widget> children;
+  final String emptyMessage;
 
-  PageContent({@required this.tabs, @required this.children});
+  PageContent({
+    @required this.tabs,
+    @required this.children,
+    this.emptyMessage,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
-          child: Container(
-            height: 50.0,
-            child: TabBar(
-              unselectedLabelColor: Colors.grey,
-              labelColor: Theme.of(context).primaryColor,
-              tabs: tabs,
+    return Provider<String>.value(
+      value: emptyMessage,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: Container(
+              height: 50.0,
+              child: TabBar(
+                unselectedLabelColor: Colors.grey,
+                labelColor: Theme.of(context).primaryColor,
+                tabs: tabs,
+              ),
             ),
           ),
-        ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: children,
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: children,
+          ),
         ),
       ),
     );
