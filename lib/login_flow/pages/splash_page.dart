b1937c2cc258bf48d7377cbee37a45453bb6b399
albert <albertmasa2@gmail.com>
2020-05-18 17:01:56 +0200

    Wrapped home content with a Scaffold
    Fixes missing Scaffold alert
    
    Co-authored-by: veralegret <veralegret@gmail.com>

--- lib/login_flow/pages/splash_page.dart
+++ lib/login_flow/pages/splash_page.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   final String error;
-  const SplashPage({ this.error });
+  const SplashPage({this.error});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: error != null ? Text(error) : CircularProgressIndicator(),
+      home: Scaffold(
+        body: Center(
+          child: error != null ? Text(error) : CircularProgressIndicator(),
+        ),
       ),
     );
   }
