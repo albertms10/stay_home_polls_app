ca36323b028c4a0757c7450f395e452c4167f111
albert <albertmasa2@gmail.com>
2020-05-18 17:54:44 +0200

    Refactored Feed and User Content in a new PageContent Widget
    Co-authored-by: veralegret <veralegret@gmail.com>

---
+++ lib/pages/page_content.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PageContent extends StatelessWidget {
+  final tabs;
+  final children;
+
+  PageContent({@required this.tabs, @required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            height: 50.0,
+            child: TabBar(
+              unselectedLabelColor: Colors.grey,
+              labelColor: Theme.of(context).primaryColor,
+              tabs: tabs,
+            ),
+          ),
+        ),
+        body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          children: children,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/user_content.dart
+++ lib/pages/user_content.dart
@@ -1,56 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
+import 'package:stay_home_polls_app/pages/page_content.dart';
 import 'package:stay_home_polls_app/widgets/polls_container.dart';
 
 class UserContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
-          child: Container(
-            height: 50.0,
-            child: TabBar(
-              unselectedLabelColor: Colors.grey,
-              labelColor: Theme.of(context).primaryColor,
-              tabs: [
-                Tab(text: "POSTED"),
-                Tab(text: "VOTED"),
-              ],
-            ),
-          ),
+    return PageContent(
+      tabs: [
+        Tab(text: "POSTED"),
+        Tab(text: "VOTED"),
+      ],
+      children: [
+        PollsContainer(
+          streamPollsList: popularPollListSnapshots(),
+          filterCallback: (pollsList, userPollsList) => pollsList.where((poll) {
+            final userPoll = userPollsList.firstWhere(
+                (userPoll) => userPoll != null
+                    ? poll.id == userPoll.id && userPoll.isAuth
+                    : false,
+                orElse: () {});
+            return userPoll == null ? false : true;
+          }).toList(),
         ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            PollsContainer(
-              streamPollsList: popularPollListSnapshots(),
-              filterCallback: (pollsList, userPollsList) =>
-                  pollsList.where((poll) {
-                final userPoll = userPollsList.firstWhere(
-                    (userPoll) => userPoll != null
-                        ? poll.id == userPoll.id && userPoll.isAuth
-                        : false,
-                    orElse: () {});
-                return userPoll == null ? false : true;
-              }).toList(),
-            ),
-            PollsContainer(
-              streamPollsList: popularPollListSnapshots(),
-              filterCallback: (pollsList, userPollsList) =>
-                  pollsList.where((poll) {
-                final userPoll = userPollsList.firstWhere(
-                    (userPoll) =>
-                        userPoll != null ? poll.id == userPoll.id : false,
-                    orElse: () {});
-                return userPoll == null ? false : true;
-              }).toList(),
-            ),
-          ],
+        PollsContainer(
+          streamPollsList: popularPollListSnapshots(),
+          filterCallback: (pollsList, userPollsList) => pollsList.where((poll) {
+            final userPoll = userPollsList.firstWhere(
+                (userPoll) => userPoll != null ? poll.id == userPoll.id : false,
+                orElse: () {});
+            return userPoll == null ? false : true;
+          }).toList(),
         ),
-      ),
+      ],
     );
   }
 }
