eb76560957581aab1b1c5a710d8e76b98e458892
albert <albertmasa2@gmail.com>
2020-05-18 21:44:05 +0200

    Fixed Scaffold.of() context issue
    Fixes #5

--- lib/login_flow/signin_flow_app.dart
+++ lib/login_flow/signin_flow_app.dart
@@ -4,6 +4,11 @@ import 'package:stay_home_polls_app/login_flow/pages/signin_page.dart';
 class SignInFlowApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SignInPage());
+    return MaterialApp(
+      home: Scaffold(
+        body: SignInPage(),
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
