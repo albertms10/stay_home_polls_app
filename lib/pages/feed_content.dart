fa608b32f901e1355d31c5e42fe80221fddb3d4c
albert <albertmasa2@gmail.com>
2020-05-18 17:54:44 +0200

    Refactored Feed and User Content in a new PageContent Widget
    Co-authored-by: veralegret <veralegret@gmail.com>

--- lib/pages/feed_content.dart
+++ lib/pages/feed_content.dart
@@ -1,47 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:stay_home_polls_app/pages/page_content.dart';
 import 'package:stay_home_polls_app/widgets/polls_container.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
 
 class FeedContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
-          child: Container(
-            height: 50.0,
-            child: TabBar(
-              unselectedLabelColor: Colors.grey,
-              labelColor: Theme.of(context).primaryColor,
-              tabs: [
-                Tab(text: "POPULAR"),
-                Tab(text: "LATEST"),
-              ],
-            ),
-          ),
+    return PageContent(
+      tabs: [
+        Tab(text: "POPULAR"),
+        Tab(text: "LATEST"),
+      ],
+      children: [
+        PollsContainer(
+          streamPollsList: popularPollListSnapshots(),
+          filterCallback: (pollsList, userPollsList) => pollsList.where((poll) {
+            final userPoll = userPollsList.firstWhere(
+                (userPoll) => userPoll != null ? poll.id == userPoll.id : false,
+                orElse: () {});
+            return userPoll == null ? true : false;
+          }).toList(),
         ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            PollsContainer(
-              streamPollsList: popularPollListSnapshots(),
-              filterCallback: (pollsList, userPollsList) =>
-                  pollsList.where((poll) {
-                final userPoll = userPollsList.firstWhere(
-                    (userPoll) =>
-                        userPoll != null ? poll.id == userPoll.id : false,
-                    orElse: () {});
-                return userPoll == null ? true : false;
-              }).toList(),
-            ),
-            PollsContainer(
-              streamPollsList: latestPollListSnapshots(),
-            ),
-          ],
+        PollsContainer(
+          streamPollsList: latestPollListSnapshots(),
         ),
-      ),
+      ],
     );
   }
 }
