5fdf462853b7b7d96d86172203f31ce71865b0f8
albert <albertmasa2@gmail.com>
2020-05-27 20:20:16 +0200

    Refactored PollTypeToggleButton Widget

--- lib/widgets/poll_type_toggle_buttons.dart
+++ lib/widgets/poll_type_toggle_buttons.dart
@@ -12,35 +12,41 @@ class PollTypeToggleButtons extends StatelessWidget {
       selectedBorderColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(4),
       children: <Widget>[
-        Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 32,
-            vertical: 16,
-          ),
-          child: Column(
-            children: <Widget>[
-              Icon(Icons.tune),
-              SizedBox(height: 4),
-              Text('Slider'),
-            ],
-          ),
+        PollTypeToggleButton(
+          icon: Icon(Icons.tune),
+          title: Text('Slider'),
         ),
-        Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 32,
-            vertical: 16,
-          ),
-          child: Column(
-            children: <Widget>[
-              Icon(Icons.format_list_bulleted),
-              SizedBox(height: 4),
-              Text('Choice'),
-            ],
-          ),
-        )
+        PollTypeToggleButton(
+          icon: Icon(Icons.format_list_bulleted),
+          title: Text('Choice'),
+        ),
       ],
       onPressed: onPressed,
       isSelected: isSelected,
+    );
+  }
+}
+
+class PollTypeToggleButton extends StatelessWidget {
+  final Text title;
+  final Icon icon;
+
+  const PollTypeToggleButton({@required this.title, @required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 32,
+        vertical: 16,
+      ),
+      child: Column(
+        children: <Widget>[
+          icon,
+          SizedBox(height: 4),
+          title,
+        ],
+      ),
     );
   }
 }
