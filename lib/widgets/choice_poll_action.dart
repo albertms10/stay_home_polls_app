ede86557fa33edf42917de3517aead3a1821fc63
Marie <veralegret@gmail.com>
2020-06-01 11:59:16 +0200

    Styling of the choice poll answered

--- lib/widgets/choice_poll_action.dart
+++ lib/widgets/choice_poll_action.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
 import 'package:stay_home_polls_app/model/user.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class ChoicePollAction extends StatefulWidget {
   final ChoicePoll choicePoll;
@@ -38,20 +39,63 @@ class _ChoicePollActionState extends State<ChoicePollAction> {
 
   @override
   Widget build(BuildContext context) {
+    List<double> votes = new List(4);
+
+    for (int i = 0; i < widget.choicePoll.optionsVoteCount.length; i++)
+      votes[i] =
+          widget.choicePoll.optionsVoteCount[i] / widget.choicePoll.totalCount;
+
     return Column(
       children: <Widget>[
         for (int i = 0; i < widget.choicePoll.optionsVoteCount.length; i++)
-          RadioListTile<int>(
-            title: Text(widget.choicePoll.options[i]),
-            secondary: voted
-                ? Text(
-                    '${(widget.choicePoll.optionsVoteCount[i] / widget.choicePoll.totalCount * 100).round()}%')
-                : null,
-            value: i,
-            groupValue: _selectedValue,
-            onChanged: _vote(),
-          )
+          voted
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: LinearPercentIndicator(
+                    /* trailing: Text(
+                        '${(widget.choicePoll.optionsVoteCount[i] / widget.choicePoll.totalCount * 100).round()}%'), */
+                    animation: true,
+                    lineHeight: 40.0,
+                    animationDuration: 2000,
+                    percent: votes[i],
+                    backgroundColor: Colors.teal[50],
+                    center: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          widget.choicePoll.options[i],
+                        ),
+                        Text(
+                          '${(widget.choicePoll.optionsVoteCount[i] / widget.choicePoll.totalCount * 100).round()}%',
+                        ),
+                      ],
+                    ),
+                    linearStrokeCap: LinearStrokeCap.roundAll,
+                    progressColor: Colors.teal[(votes[i] * 10).round() * 100],
+                  ),
+                )
+              : RadioListTile<int>(
+                  title: Text(widget.choicePoll.options[i]),
+                  secondary: voted
+                      ? Text(
+                          '${(widget.choicePoll.optionsVoteCount[i] / widget.choicePoll.totalCount * 100).round()}%')
+                      : null,
+                  value: i,
+                  groupValue: _selectedValue,
+                  onChanged: _vote(),
+                ),
       ],
     );
   }
 }
+
+/* new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2500,
+                percent: 0.8,
+                center: Text("80.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.green,
+              ), */
