b7e4817ce26623753506366cc36d9ce81d5dc5c5
Marie <veralegret@gmail.com>
2020-06-03 12:37:48 +0200

    Styling of slider poll

--- lib/widgets/slider_poll_action.dart
+++ lib/widgets/slider_poll_action.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stay_home_polls_app/model/poll.dart';
 import 'package:stay_home_polls_app/model/user.dart';
+import 'package:stay_home_polls_app/widgets/slider_custom.dart';
 
 class SliderPollAction extends StatefulWidget {
   final SliderPoll sliderPoll;
@@ -38,14 +39,52 @@ class _SliderPollActionState extends State<SliderPollAction> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Slider(
-          value: voteValue,
-          onChanged: _vote(),
-          onChangeEnd: _voted,
-          min: 0,
-          max: 100,
-          divisions: 100,
-          label: '${voteValue.floor()}',
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+          child: Container(
+            width: double.infinity,
+            height: 50,
+            decoration: new BoxDecoration(
+              borderRadius: new BorderRadius.all(
+                Radius.circular(25),
+              ),
+              gradient: new LinearGradient(
+                  colors: [
+                    Colors.teal[200],
+                    Colors.teal[400],
+                  ],
+                  begin: const FractionalOffset(0.0, 0.0),
+                  end: const FractionalOffset(1.0, 1.00),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp),
+            ),
+            child: SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                activeTrackColor: Colors.white.withOpacity(1),
+                inactiveTrackColor: Colors.white.withOpacity(.3),
+                trackHeight: 4.0,
+                thumbShape: CustomSliderThumbCircle(
+                  thumbRadius: 40 * .4,
+                  min: 1,
+                  max: 100,
+                ),
+                overlayColor: Colors.white.withOpacity(.4),
+                valueIndicatorColor: Colors.white,
+                activeTickMarkColor: Colors.white,
+                inactiveTickMarkColor: Colors.red.withOpacity(.7),
+                disabledActiveTrackColor: Colors.white.withOpacity(.8),
+                disabledInactiveTrackColor: Colors.white.withOpacity(.3),
+              ),
+              child: Slider(
+                value: voteValue,
+                onChanged: _vote(),
+                onChangeEnd: _voted,
+                min: 0,
+                max: 100,
+                divisions: 100,
+              ),
+            ),
+          ),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
