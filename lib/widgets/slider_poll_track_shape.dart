f6322f734b84f05ffa81d2bff64d1548b7568ff6
albert <albertmasa2@gmail.com>
2020-06-10 22:24:44 +0200

    feat: add custom SliderPollTrackShape
    Closes #1
    
    Co-authored-by: veralegret <veralegret@gmail.com>

---
+++ lib/widgets/slider_poll_track_shape.dart
@@ -0,0 +1,132 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class SliderPollTrackShape extends SliderTrackShape {
+  final double average;
+
+  SliderPollTrackShape({this.average});
+
+  @override
+  Rect getPreferredRect({
+    RenderBox parentBox,
+    Offset offset = Offset.zero,
+    SliderThemeData sliderTheme,
+    bool isEnabled,
+    bool isDiscrete,
+  }) {
+    final double thumbWidth =
+        sliderTheme.thumbShape.getPreferredSize(true, isDiscrete).width;
+    final double trackHeight = sliderTheme.trackHeight;
+    assert(thumbWidth >= 0);
+    assert(trackHeight >= 0);
+    assert(parentBox.size.width >= thumbWidth);
+    assert(parentBox.size.height >= trackHeight);
+
+    final double trackLeft = offset.dx + thumbWidth / 2;
+    final double trackTop =
+        offset.dy + (parentBox.size.height - trackHeight) / 2;
+    final double trackWidth = parentBox.size.width - thumbWidth;
+    return Rect.fromLTWH(trackLeft, trackTop, trackWidth, trackHeight);
+  }
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset offset, {
+    @required RenderBox parentBox,
+    @required SliderThemeData sliderTheme,
+    @required Animation<double> enableAnimation,
+    @required TextDirection textDirection,
+    @required Offset thumbCenter,
+    bool isDiscrete = false,
+    bool isEnabled = false,
+  }) {
+    assert(context != null);
+    assert(offset != null);
+    assert(parentBox != null);
+    assert(sliderTheme != null);
+    assert(sliderTheme.disabledActiveTrackColor != null);
+    assert(sliderTheme.disabledInactiveTrackColor != null);
+    assert(sliderTheme.activeTrackColor != null);
+    assert(sliderTheme.inactiveTrackColor != null);
+    assert(sliderTheme.thumbShape != null);
+    assert(enableAnimation != null);
+    assert(textDirection != null);
+    assert(thumbCenter != null);
+
+    if (sliderTheme.trackHeight <= 0) return;
+
+    final ColorTween activeTrackColorTween = ColorTween(
+        begin: sliderTheme.disabledActiveTrackColor,
+        end: sliderTheme.activeTrackColor);
+    final ColorTween inactiveTrackColorTween = ColorTween(
+        begin: sliderTheme.disabledInactiveTrackColor,
+        end: sliderTheme.inactiveTrackColor);
+    final Paint activePaint = Paint()
+      ..color = activeTrackColorTween.evaluate(enableAnimation);
+    final Paint inactivePaint = Paint()
+      ..color = inactiveTrackColorTween.evaluate(enableAnimation);
+    Paint leftTrackPaint;
+    Paint rightTrackPaint;
+    switch (textDirection) {
+      case TextDirection.ltr:
+        leftTrackPaint = activePaint;
+        rightTrackPaint = inactivePaint;
+        break;
+      case TextDirection.rtl:
+        leftTrackPaint = inactivePaint;
+        rightTrackPaint = activePaint;
+        break;
+    }
+
+    final Rect trackRect = getPreferredRect(
+      parentBox: parentBox,
+      offset: offset,
+      sliderTheme: sliderTheme,
+      isEnabled: isEnabled,
+      isDiscrete: isDiscrete,
+    );
+
+    final Rect leftTrackArcRect = Rect.fromLTWH(
+        trackRect.left, trackRect.top, trackRect.height, trackRect.height);
+    if (!leftTrackArcRect.isEmpty)
+      context.canvas.drawArc(
+          leftTrackArcRect, math.pi / 2, math.pi, false, leftTrackPaint);
+    final Rect rightTrackArcRect = Rect.fromLTWH(
+        trackRect.right - trackRect.height / 2,
+        trackRect.top,
+        trackRect.height,
+        trackRect.height);
+    if (!rightTrackArcRect.isEmpty)
+      context.canvas.drawArc(
+          rightTrackArcRect, -math.pi / 2, math.pi, false, rightTrackPaint);
+
+    final Size thumbSize =
+        sliderTheme.thumbShape.getPreferredSize(isEnabled, isDiscrete);
+    final Rect leftTrackSegment = Rect.fromLTRB(
+        trackRect.left + trackRect.height / 2,
+        trackRect.top,
+        thumbCenter.dx - thumbSize.width / 2,
+        trackRect.bottom);
+    if (!leftTrackSegment.isEmpty)
+      context.canvas.drawRect(leftTrackSegment, leftTrackPaint);
+    final Rect rightTrackSegment = Rect.fromLTRB(
+        thumbCenter.dx + thumbSize.width / 2,
+        trackRect.top,
+        trackRect.right,
+        trackRect.bottom);
+    if (!rightTrackSegment.isEmpty)
+      context.canvas.drawRect(rightTrackSegment, rightTrackPaint);
+
+    if (average != null)
+      context.canvas.drawCircle(
+        Offset(
+          trackRect.left + average * (trackRect.right - trackRect.left) / 100,
+          trackRect.top + trackRect.height / 2,
+        ),
+        4,
+        Paint()..color = Colors.orange[300],
+      );
+  }
+}
