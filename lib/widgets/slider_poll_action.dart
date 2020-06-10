0aef4ca9f02ea935de719a8832db4b7fd9a570ce
albert <albertmasa2@gmail.com>
2020-06-10 22:42:56 +0200

    refactor: propperly manage slider when voted

--- lib/widgets/slider_poll_action.dart
+++ lib/widgets/slider_poll_action.dart
@@ -45,7 +45,9 @@ class _SliderPollActionState extends State<SliderPollAction> {
   Widget build(BuildContext context) {
     SliderThemeData _baseTheme = SliderTheme.of(context).copyWith(
       trackHeight: 4.0,
-      trackShape: SliderPollTrackShape(average: widget.sliderPoll.voteAverage),
+      trackShape: SliderPollTrackShape(
+        average: voted ? widget.sliderPoll.voteAverage : null,
+      ),
       thumbShape: SliderPollThumbCircle(
         thumbRadius: 40 * .4,
         min: 1,
@@ -78,39 +80,44 @@ class _SliderPollActionState extends State<SliderPollAction> {
         disabledInactiveTrackColor: Colors.teal.withOpacity(.3),
       );
 
+    SliderTheme _slider = SliderTheme(
+      data: _data,
+      child: Slider(
+        value: voteValue,
+        onChanged: _vote(),
+        onChangeEnd: _voted,
+        min: 0,
+        max: 100,
+        divisions: 100,
+      ),
+    );
+
     return Column(
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
-          child: Container(
-            width: double.infinity,
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(25),
-              ),
-              gradient: LinearGradient(
-                  colors: [
-                    Colors.teal[200],
-                    Colors.teal[400],
-                  ],
-                  begin: const FractionalOffset(0.0, 0.0),
-                  end: const FractionalOffset(1.0, 1.00),
-                  stops: [0.0, 1.0],
-                  tileMode: TileMode.clamp),
-            ),
-            child: SliderTheme(
-              data: _data,
-              child: Slider(
-                value: voteValue,
-                onChanged: _vote(),
-                onChangeEnd: _voted,
-                min: 0,
-                max: 100,
-                divisions: 100,
-              ),
-            ),
-          ),
+          child: voted
+              ? Container(
+                  width: double.infinity,
+                  height: 41,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25),
+                    ),
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.teal[200],
+                        Colors.teal[400],
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 1.00),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp,
+                    ),
+                  ),
+                  child: _slider,
+                )
+              : _slider,
         ),
         if (voted)
           Text(
